a9c90d8bd40f93738903cce687aeb7515cfcc2c4
Sreenath H B <ubshreenath@gmail.com>
2026-05-06 11:51:03 +0530

    Remove Modernizr and jQuery UI datepicker — all modern browsers support native date inputs

--- scripts/addDatePicker.js
+++ scripts/addDatePicker.js
@@ -1,41 +1,16 @@
-﻿/// <reference path="../typings/jquery/jquery.d.ts"/>
-/// <reference path="moment.min.js"/>
-Modernizr.load({
-    test: Modernizr.inputtypes.date,
-    nope: ['../styles/jquery-ui.min.css', '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js'],
-    complete: function () {
-        if (!Modernizr.inputtypes.date) {
-            $('input[type=date]').datepicker({
-                changeMonth: true,
-                changeYear: true
-            });
-        }
-    }
-});
-
-
-
 $(function () {
 
     $(".lnkToday").click(function () {
         var d = new Date();
         var dd = ("0" + d.getDate()).slice(-2);
         var mm = ("0" + (d.getMonth() + 1)).slice(-2);
-        if (Modernizr.inputtypes.date) {
-            $(this).parent("div").prev("div").children("input").val(d.getFullYear() + "-" + mm + "-" + dd);
-            //$(this).prev("input").val(d.dateString);
-        }
-        else {
-            $(this).parent("div").prev("div").children("input").val((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear());
-        }
+        $(this).parent("div").prev("div").children("input").val(d.getFullYear() + "-" + mm + "-" + dd);
     });
-
 
     $("#btnCalculate1").click(function () {
         var dt1 = moment($("#dtFirst").val());
         var dt2 = moment($("#dtSecond").val());
         var span = Math.abs(dt2.diff(dt1));
-        //console.log(span);
         var seconds = span / 1000;
         var minutes = span / (1000 * 60);
         var hours = span / (1000 * 60 * 60);
@@ -46,17 +21,18 @@ $(function () {
 
         var friendly = humanSpan(dt1, dt2);
         $("#pnlResult").html("<p><strong>" + friendly + "</strong></p><hr/><p>Seconds : " + seconds + "</p><p>Minutes : " + minutes + "</p><p>Hours : " + hours + "</p><p>Days : " + days + "</p><p>Weeks : " + weeks + "</p><p>Months : " + months + "</p><p>Years : " + years + "</p>")
-						.parent()
-						.hide()
-						.removeClass("hide")
-						.slideDown("slow", function () {
-							$("html, body").animate({scrollTop: $("#pnlResult").offset().top}, 500);
-						});
+                        .parent()
+                        .hide()
+                        .removeClass("hide")
+                        .slideDown("slow", function () {
+                            $("html, body").animate({scrollTop: $("#pnlResult").offset().top}, 500);
+                        });
     });
+
     $("#btnCalculate2").click(function () {
         var dt1 = moment($("#dtMain").val());
         var temp = moment($("#dtMain").val());
-        
+
         var addsub = ($(":radio[name='addsub']:checked").val() == "sub") ? 0 : 1;
         var n = $("#n").val();
         var unit = $("#f").val();
@@ -68,10 +44,9 @@ $(function () {
                 dt1.subtract(n, unit);
                 break;
         }
-        //console.log(dt2.calendar());
-        $("#pnlResult2hdr").html(n + " " + unit + (addsub==0?" before ":" from ") + temp.calendar() + " is: ");
+        $("#pnlResult2hdr").html(n + " " + unit + (addsub == 0 ? " before " : " from ") + temp.calendar() + " is: ");
         $("#pnlResult2").html(dt1.calendar() + " ( " + dt1.format("dddd, MMMM Do YYYY") + " )").parent().hide().removeClass("hide").slideDown("slow", function () {
-			$("html, body").animate({scrollTop: $("#pnlResult2hdr").offset().top}, 500);
-		});
+            $("html, body").animate({scrollTop: $("#pnlResult2hdr").offset().top}, 500);
+        });
     });
 });
